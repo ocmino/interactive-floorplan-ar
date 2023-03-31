627783bb009d54ef08f191d0c70699d8d7dc4198
ocmino <ocmino@gmail.com>
2023-03-31 13:13:37 +0200

    fix mobile view for buttons

--- src/App.js
+++ src/App.js
@@ -247,7 +247,7 @@ export default function App() {
       <Modal opened={opened} onClose={close} fullScreen>
         <model-viewer
           style={{ width: "100vw", height: "80vh" }}
-          src="./Models/Apartment2.glb"
+          src="./Models/Apartment3.glb"
           alt="A 3D model of an apartment"
           auto-rotate
           camera-controls
